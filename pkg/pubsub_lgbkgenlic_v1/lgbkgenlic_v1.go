daaece380d5e8d34b08bf03c6c94b6f608f087b2
Paul Sanderson <paul@creativetech.io>
2019-04-17 11:38:14 +1200

    Adds topic to pub msg

--- pkg/pubsub_lgbkgenlic_v1/lgbkgenlic_v1.go
+++ pkg/pubsub_lgbkgenlic_v1/lgbkgenlic_v1.go
@@ -6,6 +6,7 @@ package pubsub_lgbkgenlic_v1
 var Topic = "generate_logbook_licence_v1"
 
 type GenerateLogbookLicenceMsgV1 struct {
+	Topic          string `json:"topic"`
 	UserId         string `json:"userId"`
 	IssuerId       string `json:"issuerId"`
 	SubscriptionId string `json:"subscriptionId"`
