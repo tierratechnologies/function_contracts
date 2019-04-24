54edd3ab04d2b008ba58a630cb22c3db4466bb84
Paul Sanderson <paul@creativetech.io>
2019-04-24 12:10:08 +1200

    Adds Subscription type to the pub sub contract

--- pkg/pubsub_lgbkgenlic_v1/lgbkgenlic_v1.go
+++ pkg/pubsub_lgbkgenlic_v1/lgbkgenlic_v1.go
@@ -5,10 +5,19 @@ package pubsub_lgbkgenlic_v1
 
 var Topic = "generate_logbook_licence_v1"
 
+type SubscriptionType string
+
+const (
+	TsOperatorSub SubscriptionType = "transportOp"
+	DriverSub     SubscriptionType = "driver"
+)
+
 type GenerateLogbookLicenceMsgV1 struct {
-	Topic          string `json:"topic"`
-	UserId         string `json:"userId"`
-	IssuerId       string `json:"issuerId"`
-	SubscriptionId string `json:"subscriptionId"`
-	AddonAssigned  bool   `json:"addonAssigned"`
+	Topic            string           `json:"topic"`
+	UserId           string           `json:"userId"`
+	IssuerId         string           `json:"issuerId"`
+	SubscriptionId   string           `json:"subscriptionId"`
+	SubscriptionType SubscriptionType `json:"subscriptionType"`
+	AccountName      string           `json:"accountName"`
+	AddonAssigned    bool             `json:"addonAssigned"`
 }
