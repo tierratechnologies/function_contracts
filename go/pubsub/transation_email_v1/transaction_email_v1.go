c0bbb875a820eebf4a734350d12237f1bbd9ef4f
red010b37 <red010b37@users.noreply.github.com>
2020-03-02 17:05:35 +1300

    update the type

--- go/pubsub/transation_email_v1/transaction_email_v1.go
+++ go/pubsub/transation_email_v1/transaction_email_v1.go
@@ -3,11 +3,11 @@ package transation_email_v1
 var Topic = "email_deliverability_v1"
 
 type TransactionEmailData struct {
-	Topic        string      `json:"topic,omitempty"`
-	TemplateName string      `json:"templateName,omitempty"`
-	To           string      `json:"to,omitempty"`
-	From         string      `json:"from"`
-	Subject      string      `json:"subject,omitempty"`
-	TestMode     bool        `json:"testMode"`
-	Variables    interface{} `json:"variables"`
+	Topic        string                 `json:"topic,omitempty"`
+	TemplateName string                 `json:"templateName,omitempty"`
+	To           string                 `json:"to,omitempty"`
+	From         string                 `json:"from"`
+	Subject      string                 `json:"subject,omitempty"`
+	TestMode     bool                   `json:"testMode"`
+	Variables    map[string]interface{} `json:"variables"`
 }
