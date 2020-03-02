5d06223a7217b1d8a0f79cd8ff06c4564476c94d
red010b37 <red010b37@users.noreply.github.com>
2020-03-02 16:21:08 +1300

    Adds transaction_email contract

---
+++ go/pubsub/transation_email_v1/transaction_email_v1.go
@@ -0,0 +1,13 @@
+package transation_email_v1
+
+var Topic = "email_deliverability_v1"
+
+type EmailDeliverabilityData struct {
+	Topic        string      `json:"topic,omitempty"`
+	TemplateName string      `json:"templateName,omitempty"`
+	To           string      `json:"to,omitempty"`
+	From         string      `json:"from"`
+	Subject      string      `json:"subject,omitempty"`
+	TestMode     bool        `json:"testMode"`
+	Variables    interface{} `json:"variables"`
+}
