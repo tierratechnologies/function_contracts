c2614d23ed7c2c0b89ccea3ce241e04eebd5cd65
red010b37 <red010b37@users.noreply.github.com>
2020-03-04 17:20:53 +1300

    Updates SendEmailMsgV1 Contract

--- go/pubsub_mailgun_v1/pubsub_mailgun_v1.go
+++ go/pubsub_mailgun_v1/pubsub_mailgun_v1.go
@@ -3,10 +3,10 @@ package pubsub_mailgun_v1
 var Topic = "send_email_v1"
 
 type SendEmailMsgV1 struct {
-	Topic string `json:"topic"`
-
-	To      string `json:"to"`
-	From    string `json:"from"`
-	Subject string `json:"subject"`
-	Text    string `json:"message"`
+	Topic                string `json:"topic"`
+	To                   string `json:"to"`
+	From                 string `json:"from"`
+	Subject              string `json:"subject"`
+	Text                 string `json:"message"`
+	TransactionEmailPath string `json:"transaction_email_path"` // the location of the orgina
 }
