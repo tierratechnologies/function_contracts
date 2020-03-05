9cf13fe72d4cbfe36050ad3434dc21970330c1bf
red010b37 <red010b37@users.noreply.github.com>
2020-03-05 17:39:16 +1300

    updates contract

--- go/pubsub/transaction_email_v1/transaction_email_v1.go
+++ go/pubsub/transaction_email_v1/transaction_email_v1.go
@@ -3,14 +3,14 @@ package transaction_email_v1
 var Topic = "transaction_email_v1"
 
 type TransactionEmailData struct {
-	Topic              string      `json:"topic,omitempty"`
-	TemplateName       string      `json:"template_name,omitempty"`
-	To                 string      `json:"to,omitempty"`
-	From               string      `json:"from"`
-	ReceiverFirstName  string      `json:"receiver_first_name,omitempty"`
-	ReceiverFamilyName string      `json:"receiver_family_name,omitempty"`
-	Subject            string      `json:"subject,omitempty"`
-	TestMode           bool        `json:"test_mode"`
-	Variables          interface{} `json:"variables"`
-	FSDocPath          string      `json:"fs_doc_path"` // the path where the transaction email data  is located
+	Topic              string            `json:"topic,omitempty"`
+	TemplateName       string            `json:"template_name,omitempty"`
+	To                 string            `json:"to,omitempty"`
+	From               string            `json:"from"`
+	ReceiverFirstName  string            `json:"receiver_first_name,omitempty"`
+	ReceiverFamilyName string            `json:"receiver_family_name,omitempty"`
+	Subject            string            `json:"subject,omitempty"`
+	TestMode           bool              `json:"test_mode"`
+	Variables          map[string]string `json:"variables"`
+	FSDocPath          string            `json:"fs_doc_path"` // the path where the transaction email data  is located
 }
